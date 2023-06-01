6671c572e76ca69a896f02d43a04baa4fb19f291
ferpioli <ferpiolli@gmail.com>
2023-06-01 08:51:21 -0300

    heranca

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.ferpioli.screenmatch.modelos.Filme;
+import br.com.ferpioli.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -12,6 +13,14 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("total de avaliações:" + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+
 
     }
 }
