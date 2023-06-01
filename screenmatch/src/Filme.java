f229cccea5d3cd0d9e546ed65adbca24f6b4c753
ferpioli <ferpiolli@gmail.com>
2023-06-01 07:57:12 -0300

    criacao de metodos

--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -2,7 +2,21 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("nome do filme: "  + nome);
+        System.out.println("ano de lançamento:" + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return  somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
