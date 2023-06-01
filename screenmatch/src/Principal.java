6c109f2c25b7e8da3c5b1c0f9dcb21e2d51ad784
ferpioli <ferpiolli@gmail.com>
2023-06-01 08:09:48 -0300

    encapsulamento

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -9,8 +9,7 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("total de avaliações:" + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
