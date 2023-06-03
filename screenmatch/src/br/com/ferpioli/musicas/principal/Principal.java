a2299771d6cd43746162573bcc0c64d347aee65a
ferpioli <ferpiolli@gmail.com>
2023-06-03 16:43:29 -0300

    criando metodos para curtidas e reproduces de audio

---
+++ screenmatch/src/br/com/ferpioli/musicas/principal/Principal.java
@@ -0,0 +1,32 @@
+package br.com.ferpioli.musicas.principal;
+
+import br.com.ferpioli.screenmatch.modelos.Musica;
+import br.com.ferpioli.screenmatch.modelos.PodCast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i <1000 ; i++) {
+            minhaMusica.reproduz();
+
+        }
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        PodCast meuPodCast = new PodCast();
+        meuPodCast.setTitulo("BolhaQA");
+        meuPodCast.setApresentador("Fernando");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodCast.reproduz();
+        }
+        for (int i = 0; i < 1000; i++) {
+            meuPodCast.curte();
+
+        }
+    }
+}
