057c23f5215e0fbf37ff33d22dbe092973051f73
ferpioli <ferpiolli@gmail.com>
2023-06-08 18:45:54 -0300

    criando classes

---
+++ screenmatch/src/br/com/ferpioli/desafios/desafio2/CartaoDeCredito.java
@@ -0,0 +1,26 @@
+package br.com.ferpioli.desafios.desafio2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    }
