caf87f9c3396e2d4aa92915593cc4da676f330ea
ferpioli <ferpiolli@gmail.com>
2023-06-12 08:42:48 -0300

    consumindo API

---
+++ screenmatch/src/br/com/ferpioli/screeanmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,19 @@
+package br.com.ferpioli.screeanmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://omdbapi.com/?t=matrix&apiKey=73ba8dae"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
