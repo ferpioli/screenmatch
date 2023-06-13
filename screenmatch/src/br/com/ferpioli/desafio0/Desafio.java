ad4bcfb33f08180d64a52202fd97549f27153e2d
ferpioli <ferpiolli@gmail.com>
2023-06-13 20:50:49 -0300

    inicializar dados do cliente

---
+++ screenmatch/src/br/com/ferpioli/desafio0/Desafio.java
@@ -0,0 +1,14 @@
+package br.com.ferpioli.desafio0;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Maria Bethania";
+        String tipoConta = "Corrente";
+        double saldo = 1566.99;
+
+        System.out.println("************************************************");
+        System.out.println("\n Nome do Cliente " + nome);
+        System.out.println(("Tipo conta " + tipoConta));
+        System.out.println("Saldo atual " + saldo);
+        System.out.println("\n*************************************************");    }
+}
