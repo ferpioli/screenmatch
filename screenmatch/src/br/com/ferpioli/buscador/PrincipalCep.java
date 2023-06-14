69c9b069cc0c43aee48edb61e8c159972b011f3f
ferpioli <ferpiolli@gmail.com>
2023-06-14 08:42:28 -0300

    gerando arquivo json

--- screenmatch/src/br/com/ferpioli/buscador/PrincipalCep.java
+++ screenmatch/src/br/com/ferpioli/buscador/PrincipalCep.java
@@ -1,17 +1,22 @@
 package br.com.ferpioli.buscador;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PrincipalCep {
     public static void main(String[] args) {
-
+        Scanner leitura = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
+        System.out.println("Digite o numero de CEP para consulta: ");
+        var cep = leitura.nextLine();
         try {
-            Endereco novoEmdereco = consultaCep.buscaEndereco("0");
-            System.out.println(novoEmdereco);
-        }catch (RuntimeException e ){
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        }catch (RuntimeException | IOException e ){
             System.out.println(e.getMessage());
-            System.out.println("Finalisando a aplicação");
+            System.out.println("encerra a aplicação");
         }
 
     }
